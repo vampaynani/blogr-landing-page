b340e67084c438818ce3c3006c1065c742999ac8
Gustavo Luna <gustavolunalimon@gmail.com>
2021-04-29 14:37:42 -0500

    [mod] images

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <img src="/images/bg-pattern-circles.svg" alt="title" />
+        </>
+    )
+}
+
+export default Main
